35cf989feebe88ecf1ee6162d7f7e9a206fb7952
Simeon Bensona <32086300+benson000@users.noreply.github.com>
2018-07-01 10:31:19 +0700

    Bug fixes
    Error yang diperbaiki : cuma mau input pas dirubah value di modalnya, jadi kalo inputnya sama kayak sebelumnya ga bisa
    
    Ini udah bias :^)

--- js/getDebit.js
+++ js/getDebit.js
@@ -6,75 +6,56 @@ var jumlah;
 var keterangan;
 
 var rowIndex = 1;
+/*
+		get item function
+ */
 
-$("#no_akun_debit").on('change', function(){
-	noAkun = this.value.substring(0, 5);
-	namaAkun = this.value.substring(8, this.value.length);
-});
+ function getDebit(){
+ 	//from option
+ 	noAkun = $("#no_akun_debit").val().substring(0, 5);
+ 	namaAkun = $("#no_akun_debit").val().substring(8, $("#no_akun_debit").val().length);
 
-$("#jumlah_debit").on('change', function(){
-	jumlah = this.value;
-});
+ 	jumlah = $("#jumlah_debit").val();
 
-$("#keterangan_debit").on('change', function(){
-	if(this.value != ""){
-		keterangan = this.value;
+ 	if($("#keterangan_debit").val() != ""){
+		keterangan = $("#keterangan_debit").val();
 	}else{
 		keterangan = " ";
 	}
-});
 
-function refreshTable(){
-	var counter = 0;
+	console.log(jumlah);
+ }
 
-	$('#resTable tr').each(function() { 
-	    var index = $(this).find("td").eq(0).html();
-
-	    if(index != counter && index != 1){
-	    	$(this).find("td").eq(0).html(counter); //change the id
-	    }
-
-	    counter++;
-	    rowIndex = counter;
-	});
-}
-
+ /*
+		REFRESH FUNCTIONS
+ */
 function removeRow(row){
 	$(row).parents("tr").remove();
 
 	refreshTable();
 }
 
-function createDebitRow(noAkun, namaAkun, jumlah, keterangan){
-	var row;
-	var rowContent;
+function refreshTable(){
+	var counter = 0;
 
-	row = document.getElementById("outputRow");
+	$('#resTable tr').each(function() { 
+	    var index = $(this).find("td").eq(0).html(); //get the id
 
-	rowContent = "<tr id='" + rowIndex + "'>";
-	rowContent += "<td>" + rowIndex + "</td>";
-	rowContent += "<td>" + noAkun + "</td>";
-	rowContent += "<td>" + namaAkun + "</td>";
+	    if(index != counter){
+	    	$(this).find("td").eq(0).html(counter); //change the id
 
-	rowContent += "<td>" + jumlah + "</td>";
-	rowContent += "<td>0</td>";
+	    	counter++;
+	    	rowIndex = counter;
+	    }else if(index == 0){
+	    	counter++;
 
-	rowContent += "<td>" + keterangan + "</td>";
-	rowContent += "<td>" + "<button href='#' onclick='removeRow(this); refreshTotalDebit(); onchange='refreshTotalDebit();''> Delete </button>";
+	    	$(this).find("td").eq(0).html(counter); //change the id
 
-	rowContent +="</tr>";
+	    	rowIndex = counter;
+	    }
 
-	rowIndex++;
-
-	totalDebit += jumlah; //tambah
-
-	row.insertAdjacentHTML('beforeend', rowContent);
-
-	clearDebitModalInput();
-}
-
-function isInputComplete(){
-	return noAkun.length != 0 && namaAkun.length != 0 && jumlah != null; //check the completeness of input
+	    
+	});
 }
 
 function clearVars(){
@@ -108,13 +89,56 @@ function refreshTotalDebit(){
 	$("#debitBalance").attr("readonly", true); //make it as readonly again
 }
 
+/*
+	creator function
+ */
+
+function createDebitRow(noAkun, namaAkun, jumlah, keterangan){
+	var row;
+	var rowContent;
+
+	row = document.getElementById("outputRow");
+
+	rowContent = "<tr id='" + rowIndex + "'>";
+	rowContent += "<td>" + rowIndex + "</td>";
+	rowContent += "<td>" + noAkun + "</td>";
+	rowContent += "<td>" + namaAkun + "</td>";
+
+	rowContent += "<td>" + jumlah + "</td>";
+	rowContent += "<td>0</td>";
+
+	rowContent += "<td>" + keterangan + "</td>";
+	rowContent += "<td>" + "<button href='#' onclick='removeRow(this); refreshTotalDebit();' onchange='refreshTotalDebit()';> Delete </button>";
+
+	rowContent +="</tr>";
+
+	rowIndex++;
+
+	totalDebit += jumlah; //tambah
+
+	row.insertAdjacentHTML('beforeend', rowContent);
+
+	clearDebitModalInput();
+}
+
+/* 
+*	CHECKER FUNCTIONS
+*/
+
+function isInputComplete(){
+	return jumlah != null; //check the completeness of input
+}
+
 $("#debit_submit").on('click', function(){
+	//get function
+	getDebit();
+
 	if(isInputComplete()){
+		//creator funtion
 		createDebitRow(noAkun, namaAkun, jumlah, keterangan);
 
+		//refresh function
 		clearVars();
-		
-		//renew debitBalance
 		refreshTotalDebit();
 	}else{
 		alert("Silakan isi dengan lengkap!");
