5c18071bc5194d16e3b68826a0e7f1af6600aa28
Simeon Bensona <32086300+benson000@users.noreply.github.com>
2018-07-03 10:32:13 +0700

    Add files via upload

---
+++ js/sort_table.js
@@ -0,0 +1,64 @@
+var bulan;
+var tahun;
+
+var dataTanggal = [];
+
+function clearOutputRow(){
+	$("#outputRow").empty();
+}
+
+$("#sortButton").on('click', function(){
+	clearOutputRow();
+	dataTanggal = []; //return blank array
+
+	bulan = $("#monthSelected").val();
+	tahun = $("#yearSelected").val();
+
+	dataTanggal.push({
+		month: bulan,
+		year: tahun
+	});
+
+	$.ajax({
+		url: './select_data.php',
+		data: {tanggal: JSON.stringify(dataTanggal)},
+		type: "POST",
+		success: function(returnData){
+			var outputRow = document.getElementById("outputRow");
+
+			if(returnData == "Data tidak ada"){
+				alert(returnData);
+			}else{
+				outputRow.insertAdjacentHTML("beforeEnd", returnData);
+			}
+		},
+		error: function(xhr, status, error) {
+			var err = eval("(" + xhr.responseText + ")");
+			alert(err.Message);
+		}
+	});
+});
+
+$("#showAllButton").on('click', function(){
+	clearOutputRow();
+	dataTanggal = []; //return blank array
+
+	$.ajax({
+		url: './select_data.php',
+		data: {tanggal: JSON.stringify(dataTanggal)}, //$jsonSQL[0] === false
+		type: "POST",
+		success: function(returnData){
+			var outputRow = document.getElementById("outputRow");
+
+			if(returnData == "Data tidak ada"){
+				alert(returnData);
+			}else{
+				outputRow.insertAdjacentHTML("beforeEnd", returnData);
+			}
+		},
+		error: function(xhr, status, error) {
+			var err = eval("(" + xhr.responseText + ")");
+			alert(err.Message);
+		}
+	});
+});
